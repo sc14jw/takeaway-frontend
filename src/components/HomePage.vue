112f7890dc6f56caf665e271d73df75b30bb4670
Jack Wainwright <sc14jw@leeds.ac.uk>
2018-07-23 13:56:20 +0100

    Added initial home page for the app.

---
+++ src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1> Welcome! </h1>
+        <hr>
+        <p> Welcome to the takeaway server </p>
+        <button v-on:click="newVote()"> Create a new vote </button>
+        <p> Or enter the ID of an existing vote in the box below: </p>
+        <span> <input v-model="voteId" placeholder="Vote ID"> <button v-on:click="openVote()"> Ok </button> </span>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      voteId: null
+    }
+  },
+  methods: {
+    openVote () {
+      console.log('voteId = ' + this.voteId)
+    },
+    newVote () {
+      console.log('new vote clicked.')
+    }
+  }
+}
+</script>
